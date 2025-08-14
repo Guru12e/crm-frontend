3dc9b67038a8735328323c7fb619a5d9e2a6ff37
Krithick000 <krithickprabakaran04@gmail.com>
2025-08-14 13:59:35 +0530

    update

--- app/on-boarding/page.jsx
+++ app/on-boarding/page.jsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import OnBoardingFormComponent from "@/components/OnBoardingFormComponent";
+import { supabase } from "@/utils/supabase/client";
 
 export default async function OnBoardingForm() {
   const session = await getServerSession(authOptions);
@@ -10,5 +11,18 @@ export default async function OnBoardingForm() {
     redirect(`/auth/redirect?callbackUrl=/on-boarding`);
   }
 
-  return <OnBoardingFormComponent session={session} />;
+  const userEmail = session.user.email;
+  console.log(session);
+  console.log(userEmail);
+  const { data: user, error } = await supabase
+    .from("Users")
+    .select("*")
+    .eq("email", userEmail)
+    .single();
+  console.log("User data:", user);
+  if (!user) {
+    return <OnBoardingFormComponent session={session} />;
+  } else {
+    return redirect(`/home`);
+  }
 }
